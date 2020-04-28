4e3515372d16a3dc81a85690cccec65e20965626
Divan Visagie <me@dvisagie.com>
2020-04-28 20:25:09 +0200

    cleanup

--- python/__init__.py
+++ python/__init__.py
@@ -1,6 +1,5 @@
 #! ./env/bin/python
 
-import greeter
 import json
 
 def get_valid_objects(o):
@@ -10,31 +9,27 @@ def get_valid_objects(o):
         if 'should_process' in o and o['should_process'] == True:
             valids.append(o)
         else:
-            print(f'- {o} is a dict')
             for k in o:
                 v = o[k]
                 m = get_valid_objects(v)
                 valids.extend(m)
     
     if type(o) is list:
-        print(f'- {o} is a list')
         for i in o:
             m = get_valid_objects(i)
             valids.extend(m)
     
     return valids
 
-def sumOf(o):
+def sum_search(json_string):
+    o = json.load(json_string)
     vo = get_valid_objects(o)
-    print(vo)
     values = map(lambda x: int(x['value']), vo)
     return sum(list(values))
 
 def main():
     with open('sample.json', 'r') as json_file:
-        parsed = json.load(json_file)
-        print(f'loaded json {parsed}')
-        print(f'sum: {sumOf(parsed)}')
+        print(f'sum: {sum_search(json_file)}')
     
 if __name__ == "__main__":
     main()
